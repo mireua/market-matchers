682ef7f134ada6a95b76449900c86db682c54ab7
mireu (Samuel) <103215918+mireua@users.noreply.github.com>
2023-03-26 00:03:08 +0000

    Init2

--- index.php
+++ index.php
@@ -2,34 +2,19 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Untitled Document</title>
+<title>MarketMatchers</title>
 </head>
 
 <body>
  
- <?php
-   $servername = "localhost"; 
-   $username = "root"; 
-   $password = "";
-   $dbname = "marketmatchers"; 
+<?php
+include "db.php";
 
-   $conn = mysqli_connect($servername, 
-                        $username, 
-                        $password, 
-                        $dbname);
-
-   if (!$conn) {
+//Build Query
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-   }
-   else
-   {
-     echo "Successfully Connected to Database<br/><br/>";
-   }
-
-   $sql = "SELECT * FROM accounts;"; 
-   $qryResult = mysqli_query($conn, $sql);
-	 
-    mysqli_close($conn);
+  }
+    echo "Connected successfully!";
 ?>
 
 </body>
