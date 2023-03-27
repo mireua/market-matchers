8c145f71cb086d3a6737f50aa0ab995d03bca18b
mireu (Samuel) <103215918+mireua@users.noreply.github.com>
2023-03-27 01:13:12 +0100

    Final form update before demo 27/MAR/2023

--- Library/nonFood.php
+++ Library/nonFood.php
@@ -5,12 +5,10 @@ namespace Library;
 class nonFood
 {
     public string $info;
-    public bool $flammable;
-    public bool $toxic;
 
     public function __toString(): string
     {
-        return "Product Information: " . $this->info . " Flammable: " . $this->flammable . " Toxic: " . $this->toxic;
+        return "Product Information: " . $this->info . " Flammable: ";
     }
 
 
