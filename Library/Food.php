f88e92046b743f6170d979220e02e8089ebd5a2e
mireu (Samuel) <103215918+mireua@users.noreply.github.com>
2023-04-28 17:41:08 +0100

    Login/register, product page init

--- Library/Food.php
+++ Library/Food.php
@@ -1,11 +1,43 @@
 <?php
 
 namespace Library;
+use PDO;
 
-class Food extends Products
+class Food
 {
     //info as in macros/calories
     public string $macros;
+
+    public function createFood($name, $price, $description, $macros) {
+        $db = new \db;
+        $conn = $db->connection();
+        
+        $query = $conn->prepare("INSERT INTO products VALUES (null, '$name','$price', '$description')");
+
+
+        $query2 = $conn->prepare("SELECT * FROM products WHERE itemName = '$name'");
+        $query2->execute();
+        $numrows = $query2->rowCount();
+
+        if ($numrows == 0){
+            
+            $query->execute();
+
+            $query3 = $conn->prepare("SELECT itemID FROM products WHERE itemName = '$name'");
+            $query3->execute();
+            $result = $query3->fetchColumn();
+
+            $query4 = $conn->prepare("INSERT INTO food VALUES ('$result', '$macros')");
+            $query4->execute();
+
+            echo "You have added your FOOD item!";
+
+        } else if ($numrows > 0){
+            echo "There is an existing item with this name already!";
+        } else {
+            echo "There was error creating your item!";
+        }
+    }
 
     public function __toString(): string
     {
