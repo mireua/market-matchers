a73993559559100d665a0cfc9fa82a88e5989cfb
mireu (Samuel) <103215918+mireua@users.noreply.github.com>
2023-04-29 00:28:27 +0100

    AHAHAHAHA

---
+++ Scripts/demoscript.php
@@ -0,0 +1,32 @@
+<?php
+use People\Account; // Importing the Account class from the People namespace
+
+include "db.php"; // Including the database connection file
+include "../People/Account.php"; // Including the Account class file
+
+$account = new Account(); // Creating an instance of the Account class
+
+// If the 'name' form field is submitted, get the first names of all accounts that match the provided name
+if (isset($_POST['name']))
+{
+    $name = $_POST['name'];
+    $account->getFname($name);
+}
+
+// If the 'namepre' and 'nameafter' form fields are submitted, update the first name of all accounts that match the provided 'namepre'
+if (isset($_POST['namepre']) && isset($_POST['nameafter']))
+{
+    $namepre = $_POST['namepre'];
+    $nameafter = $_POST['nameafter'];
+    $account->setFname($namepre, $nameafter);
+}
+
+// If the 'delete' form field is submitted, delete the account that matches the provided ID
+if(isset($_POST['delete']))
+{
+    $id = $_POST['delete'];
+    $account->deleteAccount((int)$id);
+}
+
+
+?>
